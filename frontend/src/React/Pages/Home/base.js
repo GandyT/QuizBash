80646a26b473365550af55db8b35549e2e4f2a58
BuildTools <randysim9@gmail.com>
2020-11-28 09:36:00 -0500

    frontend init

---
+++ frontend/src/React/Pages/Home/base.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./base.css";
+
+export default class Home extends React.Component {
+    render() {
+        // Whatever HTML for "HOME" goes in here
+        // btw to give things a class it's className and not class in react
+        return (
+            <div></div>
+        )
+    }
+}
