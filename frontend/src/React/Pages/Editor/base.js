d10a9d2fa62a0c4ab4237d828f27b5526cd124de
AntoDono <68516927+AntoDono@users.noreply.github.com>
2020-11-28 19:55:30 -0500

    merge

--- frontend/src/React/Pages/Editor/base.js
+++ frontend/src/React/Pages/Editor/base.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "./base.css";
+import Axios from "axios";
+import Session from "../../../resource/session.js";
+import { Redirect } from "react-router-dom";
 
 export default class Editor extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quizzes: undefined
+    }
+  }
+  redirect = () => {
+      if (this.state.signup)
+          return <Redirect to="/login" />
+  }
+  componentDidMount = ()=>{
+    var token = Session.getToken;
+    if (!token){
+    }
+    Axios.post("api/getuser", (token: token))
+    .then(res => {
+        if (res.success) {
+            this.state.quizzes = res.data.quizzes;
+            return this.forceUpdate();
+        }
+        this.setState({ error: res.data.error });
+    });
+  }
     render() {
+        {this.redirect()}
         return (
-            <div></div>
+            <div>`${this.state.quizzes}`</div>
         )
     }
 }
